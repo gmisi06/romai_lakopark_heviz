ee4f21fffd12ec083c9021bc79b057d708666b49
Gönye Mihály <gmisi2003@gmail.com>
2024-06-23 11:51:07 +0200

    Repair navigate to gallery button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
--- src/components/Buttons/ToGallaryButton.jsx
+++ src/components/Buttons/ToGallaryButton.jsx
@@ -9,6 +9,14 @@ function ToGallaryButton() {
     threshold: 0.4,
   });
 
+  const handleNavigate = (path) => {
+    navigate(path);
+    window.scrollTo(0, 0);
+    if (detailsRef.current) {
+      detailsRef.current.removeAttribute("open");
+    }
+  };
+
   return (
     <motion.button
       className="bg-slate-900 hover:bg-slate-700 tr text-white text-center px-2 py-1 font-semibold rounded shadow ease-in-out"
@@ -16,7 +24,7 @@ function ToGallaryButton() {
       ref={ref}
       animate={inView ? { y: 0, opacity: 1 } : { y: -20, opacity: 0 }}
       transition={{ duration: 1 }}
-      onClick={() => navigate("/galeria")}
+      onClick={() => handleNavigate("/galeria")}
     >
       Ugrás a képekhez
     </motion.button>
